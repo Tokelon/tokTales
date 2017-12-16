4650d4b1a5c5669fba36c2834196323c19651faa
Tokelon <tokelontales@gmail.com>
2017-12-16 19:32:43 +0100

    Add support for Dependency Injection via Guice
    Added Guice as dependency in tokTales-core project.
    Added IInjectConfig as the engine configuration for DI (Guice modules).
    Implementations are SimpleInjectConfig (extend) and AdvancedInjectConfig
    (extend + override).
    
    Also fixed that tokTales-core dependencies would not be passed to
    Android projects for Eclipse building.

---
+++ tokTales-core/src/main/java/com/tokelon/toktales/core/engine/IInjectConfig.java
@@ -0,0 +1,29 @@
+package com.tokelon.toktales.core.engine;
+
+import java.util.Collection;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Stage;
+import com.tokelon.toktales.core.prog.annotation.CustomFunctionalInterface;
+
+@CustomFunctionalInterface
+public interface IInjectConfig {
+    
+    public Collection<Module> getModules();
+    
+    
+    public default Injector createInjector() {
+        return Guice.createInjector(getDefaultStage(), getModules());
+    }
+    
+    public default Injector createInjector(Stage stage) {
+        return Guice.createInjector(stage, getModules());
+    }
+    
+    public default Stage getDefaultStage() {
+        return Stage.PRODUCTION;
+    }
+    
+}
---
+++ tokTales-core/src/main/java/com/tokelon/toktales/core/engine/SimpleInjectConfig.java
@@ -0,0 +1,50 @@
+package com.tokelon.toktales.core.engine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import com.google.inject.Module;
+import com.google.inject.Stage;
+
+public class SimpleInjectConfig implements IInjectConfig {
+    
+    private final Collection<Module> moduleCollection;
+    
+    private Stage defaultStage = Stage.PRODUCTION;
+    
+    
+    public SimpleInjectConfig() {
+        moduleCollection = new ArrayList<>();
+    }
+    
+    public SimpleInjectConfig(Module... modules) {
+        moduleCollection = new ArrayList<>(Arrays.asList(modules));
+    }
+    
+    public SimpleInjectConfig(Collection<Module> modules) {
+        moduleCollection = new ArrayList<>(modules);
+    }
+
+    
+    /**
+     * 
+     * @param stage
+     * @return This object.
+     */
+    public SimpleInjectConfig setDefaultStage(Stage stage) {
+        this.defaultStage = stage;
+        return this;
+    }
+    
+    @Override
+    public Stage getDefaultStage() {
+        return defaultStage;
+    }
+
+    @Override
+    public Collection<Module> getModules() {
+        return moduleCollection;
+    }
+
+}
