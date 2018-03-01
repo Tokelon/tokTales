5b8c5855adce7e1a8a3c2b9e8c9a14d6d04ac9bd
Tokelon <tokelontales@gmail.com>
2018-03-01 17:13:26 +0100

    Extend IGameStateInputHandler from IInputCallback
    This means a state input handler can be registered on a state input in a
    generic way. A new method register() implements a default for that.
    
    Also added an empty implementation EmptyGameStateInputHandler.

--- tokTales-core/src/main/java/com/tokelon/toktales/core/engine/input/IInputCallback.java
+++ tokTales-core/src/main/java/com/tokelon/toktales/core/engine/input/IInputCallback.java
@@ -1,5 +1,11 @@
 package com.tokelon.toktales.core.engine.input;
 
+/** Parent type for all input callbacks.
+ * <p>
+ * Used as generic type in core usage.
+ * In a platform specific context it will need to be cast into one of it's subtypes.
+ *
+ */
 public interface IInputCallback {
 
 	/* Add support for generic callback?
--- tokTales-core/src/main/java/com/tokelon/toktales/core/game/states/IGameStateInputHandler.java
+++ tokTales-core/src/main/java/com/tokelon/toktales/core/game/states/IGameStateInputHandler.java
@@ -1,15 +1,31 @@
 package com.tokelon.toktales.core.game.states;
 
+import com.tokelon.toktales.core.engine.input.IInputCallback;
+
 /** Receives input and processes so it gets used by a gamestate.
  * <br><br>
  * Normally uses {@link IControlScheme} and {@link IControlHandler} of the gamestate.
  */
-public interface IGameStateInputHandler {
+public interface IGameStateInputHandler extends IInputCallback {
 	
-	// would have to pass platform dependent input (or cast)
-	//public void register(IGameStateInput gamestateInput);
+	
+	/** Registers this input handler to the given state input.
+	 * <p>
+	 * The default implementation for this simply calls {@link IGameStateInput#registerInputCallback(IInputCallback)},
+	 * and returns the result.
+	 * 
+	 * @param stateInput
+	 * @return True if registration was successful, false if not.
+	 */
+	public default boolean register(IGameStateInput stateInput) {
+		return stateInput.registerInputCallback(this);
+	}
+	
 	
 	// could add generic version here -> let's not because then all implementation would have to support it
 	//public boolean invoke(String target, String action, Object... params);
-		
+	
+	
+	public static class EmptyGameStateInputHandler implements IGameStateInputHandler { }
+	
 }
