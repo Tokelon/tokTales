263c3a5baea1f1952c9b420f3950a2f813e55509
Tokelon <tokelontales@gmail.com>
2018-03-16 15:49:13 +0100

    Add tests for parameter injection on inherited methods

---
+++ tokTales-core/tools/test/java/com/tokelon/toktales/tools/inject/ParameterInjectorInheritedInjectionTest.java
@@ -0,0 +1,102 @@
+package com.tokelon.toktales.tools.inject;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.tokelon.toktales.tools.inject.InheritedParameterInjectionTargets.InjectBaseTarget.KeyEqualMethodsPrivate;
+import com.tokelon.toktales.tools.inject.InheritedParameterInjectionTargets.InjectBaseTarget.KeyWithAnnotationGeneric;
+import com.tokelon.toktales.tools.inject.InheritedParameterInjectionTargets.InjectBaseTarget.KeyWithoutAnnotationGeneric;
+import com.tokelon.toktales.tools.inject.InheritedParameterInjectionTargets.InjectInheritTarget;
+import com.tokelon.toktales.tools.inject.InjectedObjects.InjectedObject;
+
+public class ParameterInjectorInheritedInjectionTest {
+
+	
+	private InjectInheritTarget injectionTarget;
+	
+	private InjectedObject injectedObject;
+
+	private ParameterInjector unkeyedInjector;
+
+	
+	@Before
+	public void before() {
+		injectionTarget = new InjectInheritTarget();
+		injectedObject = new InjectedObject();
+		
+		unkeyedInjector = new ParameterInjector(InjectParameters.class, injectedObject);
+	}
+	
+	
+	
+	@Test
+	public void injectInto_BaseMethod_ShouldSucceed() {
+		unkeyedInjector.injectInto(injectionTarget);
+		
+		assertSame(injectedObject, injectionTarget.injectedObjectInjectBase);
+	}
+	
+	
+	@Test
+	public void injectInto_InheritedMethodWithAnnotation_ShouldInjectBaseAndInherited() {
+		unkeyedInjector.injectInto(injectionTarget);
+		
+		assertSame(injectedObject, injectionTarget.injectedObjectOverrideWithAnnotation);
+		assertSame(injectedObject, injectionTarget.injectedObjectInheritedOverrideWithAnnotation);
+	}
+	
+	@Test
+	public void injectInto_InheritedMethodWithAnnotationGeneric_ShouldInjectBaseAndInherited() {
+		List<String> obj = new ArrayList<>();
+		ParameterInjector injector = new ParameterInjector(InjectParams.forKey(KeyWithAnnotationGeneric.class), obj);
+		injector.injectInto(injectionTarget);
+		
+		assertSame(obj, injectionTarget.injectedObjectOverrideWithAnnotationGeneric);
+		assertSame(obj, injectionTarget.injectedObjectInheritedOverrideWithAnnotationGeneric);
+	}
+
+	
+	@Test
+	public void injectInto_InheritedMethodWithoutAnnotation_ShouldNotInject() {
+		unkeyedInjector.injectInto(injectionTarget);
+		
+		assertNull(injectionTarget.injectedObjectOverrideWithoutAnnotation);
+		assertNull(injectionTarget.injectedObjectInheritedOverrideWithoutAnnotation);
+	}
+	
+	@Test
+	public void injectInto_InheritedMethodWithoutAnnotationGeneric_ShouldNotInject() {
+		List<String> obj = new ArrayList<>();
+		ParameterInjector injector = new ParameterInjector(InjectParams.forKey(KeyWithoutAnnotationGeneric.class), obj);
+		injector.injectInto(injectionTarget);
+		
+		assertNull(injectionTarget.injectedObjectOverrideWithoutAnnotationGeneric);
+		assertNull(injectionTarget.injectedObjectInheritedOverrideWithoutAnnotationGeneric);
+	}
+
+	
+	@Test
+	public void injectInto_EqualMethods_ShouldInjectBaseAndInherited() {
+		ParameterInjector injector = new ParameterInjector(InjectParams.forKey(KeyEqualMethodsPrivate.class), injectedObject);
+		injector.injectInto(injectionTarget);
+		
+		assertSame(injectedObject, injectionTarget.injectedObjectEqualMethods);
+		assertSame(injectedObject, injectionTarget.injectedObjectInheritedEqualMethods);
+	}
+	
+	
+	@Test
+	public void injectInto_InheritedMethod_OnlyInvokesOnce() {
+		unkeyedInjector.injectInto(injectionTarget);
+		
+		assertEquals(1, injectionTarget.injectBaseOverrideCallCount);
+	}
+	
+}
