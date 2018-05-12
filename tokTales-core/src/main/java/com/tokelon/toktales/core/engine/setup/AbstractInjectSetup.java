64f3f2a4c15b06b8771ff491f29a2b9550666a32
Tokelon <tokelontales@gmail.com>
2018-05-12 16:44:49 +0200

    Implement running the engine setup in different modes

--- tokTales-core/src/main/java/com/tokelon/toktales/core/engine/setup/AbstractInjectSetup.java
+++ tokTales-core/src/main/java/com/tokelon/toktales/core/engine/setup/AbstractInjectSetup.java
@@ -1,6 +1,7 @@
 package com.tokelon.toktales.core.engine.setup;
 
 import com.google.inject.Injector;
+import com.google.inject.Stage;
 import com.tokelon.toktales.core.engine.EngineException;
 import com.tokelon.toktales.core.engine.IEngineContext;
 import com.tokelon.toktales.core.engine.inject.IHierarchicalInjectConfig;
@@ -9,10 +10,27 @@ import com.tokelon.toktales.core.game.IGameAdapter;
 
 public abstract class AbstractInjectSetup implements IEngineSetup {
 
+	
+	// Default setup mode is production
+	private SetupMode setupMode = SetupMode.PRODUCTION;
+	
 
+	/** Implement your custom setup logic here.
+	 * <p>
+	 * Called in {@link #run(IEngineContext)}.
+	 * 
+	 * @param context The engine context created.
+	 * @throws EngineException If there was an error while running the setup.
+	 */
+	protected abstract void doRun(IEngineContext context) throws EngineException;
+
+	
 	@Override
 	public IEngineContext create(IHierarchicalInjectConfig injectConfig) throws EngineException {
-		Injector injector = createInjector(injectConfig); 
+		System.out.println("Engine creation started");
+		System.out.println("Engine setup is running in mode " + setupMode);
+		
+		Injector injector = createInjector(injectConfig, setupMode);
 		IEngineContext engineContext = createEngineContext(injector);
 		
 		return engineContext;
@@ -26,14 +44,26 @@ public abstract class AbstractInjectSetup implements IEngineSetup {
 		createGameAdapter(context.getInjector());
 	}
 
-	protected abstract void doRun(IEngineContext context) throws EngineException;
+	
+	@Override
+	public void setSetupMode(SetupMode mode) {
+		this.setupMode = mode;
+	}
+	
+	@Override
+	public SetupMode getSetupMode() {
+		return setupMode;
+	}
 
 
-	protected Injector createInjector(IInjectConfig injectConfig) throws EngineException {
-		System.out.println("Creating engine injector.....");
+	protected Injector createInjector(IInjectConfig injectConfig, SetupMode mode) throws EngineException {
+		Stage stage = mode == SetupMode.DEVELOPMENT ? Stage.DEVELOPMENT : Stage.PRODUCTION;
+		System.out.println("Determined injector stage as " + stage);
+		
+		System.out.print("Creating engine injector..... ");
 		long before = System.currentTimeMillis();
 
-		Injector injector = injectConfig.createInjector();
+		Injector injector = injectConfig.createInjector(stage);
 		
 		System.out.printf("Injector creation took %d ms", (System.currentTimeMillis() - before));
 		System.out.println();
@@ -41,23 +71,23 @@ public abstract class AbstractInjectSetup implements IEngineSetup {
 	}
 
 	protected IEngineContext createEngineContext(Injector injector) {
-		System.out.println("Creating engine context...");
+		System.out.println("Creating engine context... ");
 		long before = System.currentTimeMillis();
 		
 		IEngineContext engineContext = injector.getInstance(IEngineContext.class);
 		
-		System.out.printf("Engine context creation took %d ms", (System.currentTimeMillis() - before));
+		System.out.printf("...engine context creation took %d ms", (System.currentTimeMillis() - before));
 		System.out.println();
 		return engineContext;
 	}
 	
 	protected IGameAdapter createGameAdapter(Injector injector) {
-		System.out.println("Creating game adapter...");
+		System.out.println("Creating game adapter... ");
 		long before = System.currentTimeMillis();
 
 		IGameAdapter gameAdapter = injector.getInstance(IGameAdapter.class);
 		
-		System.out.printf("Game adapter creation took %d ms", (System.currentTimeMillis() - before));
+		System.out.printf("...game adapter creation took %d ms", (System.currentTimeMillis() - before));
 		System.out.println();
 		return gameAdapter;
 	}
--- tokTales-core/src/main/java/com/tokelon/toktales/core/engine/setup/IEngineSetup.java
+++ tokTales-core/src/main/java/com/tokelon/toktales/core/engine/setup/IEngineSetup.java
@@ -9,12 +9,11 @@ public interface IEngineSetup {
 	
 	/** Creates the engine context.
 	 * 
-	 * @param injectConfig The inject config.
+	 * @param injectConfig The inject config that should be used.
 	 * 
 	 * @return The engine context including the engine and the game.
 	 */
 	public IEngineContext create(IHierarchicalInjectConfig injectConfig) throws EngineException;
-	
 	
 	/** Runs any logic to setup the engine and game.
 	 * <p>
@@ -24,5 +23,28 @@ public interface IEngineSetup {
 	 */
 	public void run(IEngineContext context) throws EngineException;
 	
+	
+	/** Sets the mode in which the setup should be run.
+	 * @param mode A setup mode.
+	 */
+	public void setSetupMode(SetupMode mode);
+	
+	/** Returns the mode in which the setup should be run.
+	 * @return The current setup mode.
+	 */
+	public SetupMode getSetupMode();
+	
+	
+	/** Modes in which the engine setup can be run.
+	 * <p>
+	 * Unless you need debug information you should use {@link #PRODUCTION}.
+	 * <p>
+	 * The mode dictates in which Stage the injector will be created.
+	 * 
+	 */
+	public enum SetupMode {
+		DEVELOPMENT,
+		PRODUCTION
+	}
 	
 }
