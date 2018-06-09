32f923c834ba389fd6fefc4999619fbc89720d87
Tokelon <tokelontales@gmail.com>
2018-06-09 10:36:59 +0200

    Move the content of TokelonGameStates and delete it

--- tokTales-extensions/tokTales-extensions-default-core/src/main/java/com/tokelon/toktales/extens/def/core/values/GameStateExtensionsValues.java
+++ tokTales-extensions/tokTales-extensions-default-core/src/main/java/com/tokelon/toktales/extens/def/core/values/GameStateExtensionsValues.java
@@ -8,7 +8,39 @@ public class GameStateExtensionsValues extends GameStateValues {
 		throw new UnsupportedOperationException();
 	}
 	
+	
+	
 	public static final String STATE_LOCAL_MAP = "tokelon_extensions_states_state_local_map";
 	public static final String STATE_CONSOLE = "tokelon_extensions_states_state_console";
+	
+	
+	private static final String STATE_INVENTORY = "tokelon_extensions_states_state_inventory";
+	private static final String STATE_GLOBAL_MAP = "tokelon_extensions_states_state_global_map";
+	private static final String STATE_MAIN_MENU = "tokelon_extensions_states_state_main_menu";
+	private static final String STATE_CUTSCENE = "tokelon_extensions_states_state_cutscene";
+	
+	
+
+	@SuppressWarnings("unused") // TODO: Use?
+	private enum State {
+		
+		LOCAL_MAP(STATE_LOCAL_MAP),
+		INVENTORY(STATE_INVENTORY),
+		GLOBAL_MAP(STATE_GLOBAL_MAP),
+		MAIN_MENU(STATE_MAIN_MENU),
+		CUTSCENE(STATE_CUTSCENE),
+		;
+		
+		
+		private final String stateName;
+		
+		private State(String name) {
+			stateName = name;
+		}
+		
+		public String getName() {
+			return stateName;
+		}
+	}
 	
 }
