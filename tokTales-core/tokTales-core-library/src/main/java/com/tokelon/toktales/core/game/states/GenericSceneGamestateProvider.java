8fb1e569f13ad3f360ef026c83a4fa16ebe385f7
Tokelon <tokelontales@gmail.com>
2018-11-04 16:34:34 +0100

    Fix missing types in TestCoreStateSceneTypeConstrains
    Added GenericSceneGamestateProvider that can be used as a provider for
    any BaseGamestate implementation with some scene type.

---
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/game/states/GenericSceneGamestateProvider.java
@@ -0,0 +1,40 @@
+package com.tokelon.toktales.core.game.states;
+
+import com.google.common.reflect.TypeToken;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Provider;
+
+public class GenericSceneGamestateProvider<G extends BaseGamestate<S>, S extends IGameScene> implements Provider<G> {
+
+	
+	private Injector injector;
+	
+	private final IGenericSceneGamestateFactory<G, S> gamestateFactory;
+	private final TypeToken<S> sceneTypeToken;
+
+	public GenericSceneGamestateProvider(IGenericSceneGamestateFactory<G, S> gamestateFactory, TypeToken<S> sceneTypeToken) {
+		this.gamestateFactory = gamestateFactory;
+		this.sceneTypeToken = sceneTypeToken;
+	}
+	
+	@Inject
+	protected void injectInjector(Injector injector) {
+		this.injector = injector;
+	}
+
+	
+	@Override
+	public G get() {
+		G gamestate = gamestateFactory.create(sceneTypeToken);
+		injector.injectMembers(gamestate);
+		return gamestate;
+	}
+
+	
+	public interface IGenericSceneGamestateFactory<G extends BaseGamestate<S>, S extends IGameScene> {
+		
+		public G create(TypeToken<S> sceneTypeToken);
+	}
+
+}
