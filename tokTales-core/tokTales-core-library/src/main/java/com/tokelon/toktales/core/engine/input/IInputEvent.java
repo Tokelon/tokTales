fb282be2d0311ff66a3ac03718452d53146be197
Tokelon <tokelontales@gmail.com>
2018-11-30 18:43:32 +0100

    Add IInputEvent which will replace the current input callback parameters

---
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/input/IInputEvent.java
@@ -0,0 +1,23 @@
+package com.tokelon.toktales.core.engine.input;
+
+public interface IInputEvent {
+
+	
+	//public boolean setHandled(boolean handled);
+	//public boolean isHandled();
+	
+	//public String getEventType(); // Maybe even return a class here?
+
+	//public long getEventId(); // Must be unique across different events
+	
+	//public String getSource();
+	
+	//public long getCreationTimeMillis(); // Use a datetime format?
+	//public void setCreationTimeMillis(long timeMillis);
+	
+	
+	//public boolean wasHandled();
+	//public void markHandled(String source);
+	//public List<String> getHandledSources();
+	
+}
