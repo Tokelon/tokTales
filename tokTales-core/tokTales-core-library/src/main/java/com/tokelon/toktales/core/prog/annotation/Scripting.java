c3bfd369e881b610745564f9f9e6246af982eb01
Tokelon <tokelontales@gmail.com>
2019-02-19 19:36:22 +0100

    Add scripting annotations that can be used for meta-info on a target

---
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/prog/annotation/Scripting.java
@@ -0,0 +1,25 @@
+package com.tokelon.toktales.core.prog.annotation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.TYPE_PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/** Indicates with a comment how the target is relevant for scripting.
+ */
+@Retention(SOURCE)
+@Target({ TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE, TYPE_PARAMETER, TYPE_USE })
+public @interface Scripting {
+	
+	String value() default "";
+}
---
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/prog/annotation/ScriptingAccess.java
@@ -0,0 +1,21 @@
+package com.tokelon.toktales.core.prog.annotation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/** Indicates that the target's access modifier is important for scripting access.
+ */
+@Retention(SOURCE)
+@Target({ TYPE, FIELD, METHOD, CONSTRUCTOR, ANNOTATION_TYPE, PACKAGE })
+public @interface ScriptingAccess {
+	
+	String value() default "";
+}
