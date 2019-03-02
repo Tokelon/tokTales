ee7b30b666b478ddd7fa29d6f717e511b5595496
Tokelon <tokelontales@gmail.com>
2019-03-02 16:23:50 +0100

    Allow IAssetDecoder methods to throw ContentException

--- tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/content/manage/IAssetDecoder.java
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/content/manage/IAssetDecoder.java
@@ -2,10 +2,12 @@ package com.tokelon.toktales.core.content.manage;
 
 import java.io.InputStream;
 
+import com.tokelon.toktales.core.engine.content.ContentException;
+
 public interface IAssetDecoder<T, K, O> {
 
 	
-	public T decode(InputStream inputstream, K key);
-	public T decode(InputStream inputstream, K key, O options);
+	public T decode(InputStream inputstream, K key) throws ContentException;
+	public T decode(InputStream inputstream, K key, O options) throws ContentException;
 	
 }
--- tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/content/text/CodepointDecoder.java
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/content/text/CodepointDecoder.java
@@ -3,23 +3,24 @@ package com.tokelon.toktales.core.content.text;
 import java.io.InputStream;
 
 import com.tokelon.toktales.core.content.manage.IAssetDecoder;
+import com.tokelon.toktales.core.engine.content.ContentException;
 import com.tokelon.toktales.core.util.INamedOptions;
 
 public class CodepointDecoder implements IAssetDecoder<ICodepointAsset, ICodepointKey, INamedOptions> {
 
 	
 	@Override
-	public ICodepointAsset decode(InputStream inputstream, ICodepointKey key) {
-		return decode(inputstream, key, null);
-	}
-
-	@Override
-	public ICodepointAsset decode(InputStream inputstream, ICodepointKey key, INamedOptions options) {
+	public ICodepointAsset decode(InputStream inputstream, ICodepointKey key) throws ContentException {
 		int codepoint = key.getCodepoint();
 		
 		ITextureFont font = key.getFont();
 		ICodepointAsset codepointAsset = font.getCodepointAsset(codepoint);
 		return codepointAsset;
+	}
+
+	@Override
+	public ICodepointAsset decode(InputStream inputstream, ICodepointKey key, INamedOptions options) throws ContentException {
+		return decode(inputstream, key);
 	}
 
 }
