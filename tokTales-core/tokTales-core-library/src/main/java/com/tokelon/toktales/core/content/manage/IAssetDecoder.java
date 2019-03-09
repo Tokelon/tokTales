427d0e196d31b4241265de40679b85f743c0cad2
Tokelon <tokelontales@gmail.com>
2019-03-09 17:50:21 +0100

    Convert IAssetDecoder to a functional interface

--- tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/content/manage/IAssetDecoder.java
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/content/manage/IAssetDecoder.java
@@ -3,11 +3,33 @@ package com.tokelon.toktales.core.content.manage;
 import java.io.InputStream;
 
 import com.tokelon.toktales.core.engine.content.ContentException;
+import com.tokelon.toktales.core.prog.annotation.CustomFunctionalInterface;
 
+@CustomFunctionalInterface
 public interface IAssetDecoder<T, K, O> {
 
 	
-	public T decode(InputStream inputstream, K key) throws ContentException;
+	/** Decodes an asset from the given stream.
+	 * <p>
+	 * The default implementation for this simply calls {@link #decode(InputStream, Object, Object)} with null for options.
+	 * 
+	 * @param inputstream
+	 * @param key
+	 * @return The decoded asset, or null.
+	 * @throws ContentException If an error occurs while decoding.
+	 */
+	public default T decode(InputStream inputstream, K key) throws ContentException {
+		return decode(inputstream, key, null);
+	}
+	
+	/** Decodes an asset from the given stream.
+	 * 
+	 * @param inputstream
+	 * @param key
+	 * @param options
+	 * @return The decoded asset, or null.
+	 * @throws ContentException If an error occurs while decoding.
+	 */
 	public T decode(InputStream inputstream, K key, O options) throws ContentException;
 	
 }
