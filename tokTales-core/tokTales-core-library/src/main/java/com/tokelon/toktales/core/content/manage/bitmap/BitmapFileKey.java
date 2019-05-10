772c01445d8733a35d9585ac17df2a1aa6fc8235
Tokelon <tokelontales@gmail.com>
2019-05-10 14:47:21 +0200

    Fix BitmapFileKey not implementing IBitmapAssetKey

--- tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/content/manage/bitmap/BitmapFileKey.java
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/content/manage/bitmap/BitmapFileKey.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 
 import com.tokelon.toktales.core.content.manage.files.FileKey;
 
-public class BitmapFileKey extends FileKey {
+public class BitmapFileKey extends FileKey implements IBitmapAssetKey {
 
 	
 	public BitmapFileKey(Path path) {
