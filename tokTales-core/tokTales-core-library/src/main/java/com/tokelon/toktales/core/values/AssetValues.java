ee9ca65456720443e41c659fa2b121e706be5dd6
Tokelon <tokelontales@gmail.com>
2019-06-21 16:56:32 +0200

    Add injection bindings for asset key registry default entries

---
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/values/AssetValues.java
@@ -0,0 +1,10 @@
+package com.tokelon.toktales.core.values;
+
+public class AssetValues {
+
+	private AssetValues() {}
+	
+	
+	public static final String ASSET_KEY_ID_FONT_DEFAULT = "ASSET_VALUES-ASSET_KEY_ID_FONT_DEFAULT";
+	
+}
--- tokTales-extensions/tokTales-extensions-default-core/src/main/java/com/tokelon/toktales/extens/def/core/engine/inject/CoreDefExtensInjectConfig.java
+++ tokTales-extensions/tokTales-extensions-default-core/src/main/java/com/tokelon/toktales/extens/def/core/engine/inject/CoreDefExtensInjectConfig.java
@@ -10,6 +10,8 @@ public class CoreDefExtensInjectConfig extends HierarchicalInjectConfig {
     	super.configure();
     	
     	extend(new CoreDefExtensInjectModule());
+    	
+    	extend(new CoreDefExtensGlobalRegistriesInjectModule());
     }
     
 }
