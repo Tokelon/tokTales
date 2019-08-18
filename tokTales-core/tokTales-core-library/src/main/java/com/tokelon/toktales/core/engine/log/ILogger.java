68c36fa68cd4a31cf939ebb7e812c2f1c3f73b5e
Tokelon <tokelontales@gmail.com>
2019-08-18 19:40:51 +0200

    Add more methods to ILogger, implemented around SLF4J

--- tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/log/ILogger.java
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/log/ILogger.java
@@ -1,28 +1,99 @@
 package com.tokelon.toktales.core.engine.log;
 
 public interface ILogger {
-	// TODO: Implement SLF4J compatibility
+	// TODO: Remove overloads with tag because they are inefficient
 
-	// Maybe make the logger independent of the log framework
-	// and have an emergency logger loaded when created? and after the log framework has been loaded replace that
-
-	//public void log(level)
 	
+	/* Standard Logger
+	System.out.printf("d | %s : %s", tag, message);
+	System.out.println();
+	
+	System.out.printf("d | %s : " + format.replaceAll("{}", "%s"), name, arg);
+	 */
+	
+	/*
+	public enum LogLevel {
+		ERROR,
+		INFO,
+		DEBUG,
+		TRACE
+	}
 
+
+	public void log(LogLevel level, String message);
+	public void log(LogLevel level, String tag, String message);
+
+	public void log(LogLevel level, String format, Object arg);
+	public void log(LogLevel level, String format, Object arg1, Object arg2);
+	public void log(LogLevel level, String format, Object... args);
+	public void log(LogLevel level, String message, Throwable cause);
+	public void log(LogLevel level, String tag, String format, Object arg);
+	public void log(LogLevel level, String tag, String format, Object arg1, Object arg2);
+	public void log(LogLevel level, String tag, String format, Object... args);
+	public void log(LogLevel level, String tag, String message, Throwable cause);
+	*/
+
+	
+	public String getName();
+	
+	public boolean isErrorEnabled();
+	public boolean isInfoEnabled();
+	public boolean isDebugEnabled();
+	public boolean isTraceEnabled();
+
+	
 	public void d(String message);
 	public void d(String tag, String message);
-	//public void d(String tag, String message, Object... args); // TODO: implement string.format here and replace all current usages
+
+	public void d(String format, Object arg);
+	public void d(String format, Object arg1, Object arg2);
+	public void d(String format, Object... args);
+	public void d(String message, Throwable cause);
+	public void d(String tag, String format, Object arg);
+	public void d(String tag, String format, Object arg1, Object arg2);
+	public void d(String tag, String format, Object... args);
+	public void d(String tag, String message, Throwable cause);
 	
+
 	public void e(String message);
 	public void e(String tag, String message);
-	//public void e(String tag, String message, Throwable throwable);
+	
+	public void e(String format, Object arg);
+	public void e(String format, Object arg1, Object arg2);
+	public void e(String format, Object... args);
+	public void e(String message, Throwable cause);
+	public void e(String tag, String format, Object arg);
+	public void e(String tag, String format, Object arg1, Object arg2);
+	public void e(String tag, String format, Object... args);
+	public void e(String tag, String message, Throwable cause);
+
 	
 	public void w(String message);
 	public void w(String tag, String message);
-	//public void w(String tag, String message, Throwable throwable);
+
+	public void w(String format, Object arg);
+	public void w(String format, Object arg1, Object arg2);
+	public void w(String format, Object... args);
+	public void w(String message, Throwable cause);
+	public void w(String tag, String format, Object arg);
+	public void w(String tag, String format, Object arg1, Object arg2);
+	public void w(String tag, String format, Object... args);
+	public void w(String tag, String message, Throwable cause);
+	
 	
 	public void i(String message);
 	public void i(String tag, String message);
+	
+	public void i(String format, Object arg);
+	public void i(String format, Object arg1, Object arg2);
+	public void i(String format, Object... args);
+	public void i(String message, Throwable cause);
+	public void i(String tag, String format, Object arg);
+	public void i(String tag, String format, Object arg1, Object arg2);
+	public void i(String tag, String format, Object... args);
+	public void i(String tag, String message, Throwable cause);
+
+	
 	
 	
 	public void logOnce(char loglevel, String id, String tag, String message);
--- tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/log/SLF4JLogger.java
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/log/SLF4JLogger.java
@@ -27,6 +27,32 @@ public class SLF4JLogger implements ILogger {
 	
 	
 	@Override
+	public String getName() {
+		return logger.getName();
+	}
+	
+	@Override
+	public boolean isErrorEnabled() {
+		return logger.isErrorEnabled();
+	}
+	
+	@Override
+	public boolean isInfoEnabled() {
+		return logger.isInfoEnabled();
+	}
+	
+	@Override
+	public boolean isDebugEnabled() {
+		return logger.isDebugEnabled();
+	}
+	
+	@Override
+	public boolean isTraceEnabled() {
+		return logger.isTraceEnabled();
+	}
+	
+	
+	@Override
 	public void d(String message) {
 		logger.debug("{}: {}", DEFAULT_TAG, message);
 	}
@@ -34,6 +60,46 @@ public class SLF4JLogger implements ILogger {
 	@Override
 	public void d(String tag, String message) {
 		logger.debug("{}: {}", tag, message);
+	}
+	
+	@Override
+	public void d(String format, Object arg) {
+		logger.debug(format, arg);
+	}
+	
+	@Override
+	public void d(String format, Object arg1, Object arg2) {
+		logger.debug(format, arg1, arg2);		
+	}
+	
+	@Override
+	public void d(String format, Object... args) {
+		logger.debug(format, args);
+	}
+	
+	@Override
+	public void d(String message, Throwable cause) {
+		logger.debug(message, cause);
+	}
+	
+	@Override
+	public void d(String tag, String format, Object arg) {
+		logger.debug("{}: " + format, tag, arg);
+	}
+	
+	@Override
+	public void d(String tag, String format, Object arg1, Object arg2) {
+		logger.debug("{}: " + format, tag, arg1, arg2);
+	}
+	
+	@Override
+	public void d(String tag, String format, Object... args) {
+		logger.debug(tag + ": " + format, args);
+	}
+	
+	@Override
+	public void d(String tag, String message, Throwable cause) {
+		logger.debug(tag + ": " + message, cause);
 	}
 	
 	
@@ -47,6 +113,46 @@ public class SLF4JLogger implements ILogger {
 		logger.error("{}: {}", tag, message);
 	}
 	
+	@Override
+	public void e(String format, Object arg) {
+		logger.error(format, arg);
+	}
+	
+	@Override
+	public void e(String format, Object arg1, Object arg2) {
+		logger.error(format, arg1, arg2);		
+	}
+	
+	@Override
+	public void e(String format, Object... args) {
+		logger.error(format, args);
+	}
+	
+	@Override
+	public void e(String message, Throwable cause) {
+		logger.error(message, cause);
+	}
+	
+	@Override
+	public void e(String tag, String format, Object arg) {
+		logger.error("{}: " + format, tag, arg);
+	}
+	
+	@Override
+	public void e(String tag, String format, Object arg1, Object arg2) {
+		logger.error("{}: " + format, tag, arg1, arg2);
+	}
+	
+	@Override
+	public void e(String tag, String format, Object... args) {
+		logger.error(tag + ": " + format, args);
+	}
+	
+	@Override
+	public void e(String tag, String message, Throwable cause) {
+		logger.error(tag + ": " + message, cause);
+	}
+	
 	
 	@Override
 	public void w(String message) {
@@ -58,6 +164,46 @@ public class SLF4JLogger implements ILogger {
 		logger.warn("{}: {}", tag, message);
 	}
 	
+	@Override
+	public void w(String format, Object arg) {
+		logger.warn(format, arg);
+	}
+	
+	@Override
+	public void w(String format, Object arg1, Object arg2) {
+		logger.warn(format, arg1, arg2);		
+	}
+	
+	@Override
+	public void w(String format, Object... args) {
+		logger.warn(format, args);
+	}
+	
+	@Override
+	public void w(String message, Throwable cause) {
+		logger.warn(message, cause);
+	}
+	
+	@Override
+	public void w(String tag, String format, Object arg) {
+		logger.warn("{}: " + format, tag, arg);
+	}
+	
+	@Override
+	public void w(String tag, String format, Object arg1, Object arg2) {
+		logger.warn("{}: " + format, tag, arg1, arg2);
+	}
+	
+	@Override
+	public void w(String tag, String format, Object... args) {
+		logger.warn(tag + ": " + format, args);
+	}
+	
+	@Override
+	public void w(String tag, String message, Throwable cause) {
+		logger.warn(tag + ": " + message, cause);
+	}
+	
 	
 	@Override
 	public void i(String message) {
@@ -67,6 +213,46 @@ public class SLF4JLogger implements ILogger {
 	@Override
 	public void i(String tag, String message) {
 		logger.info("{}: {}", tag, message);
+	}
+
+	@Override
+	public void i(String format, Object arg) {
+		logger.info(format, arg);
+	}
+	
+	@Override
+	public void i(String format, Object arg1, Object arg2) {
+		logger.info(format, arg1, arg2);		
+	}
+	
+	@Override
+	public void i(String format, Object... args) {
+		logger.info(format, args);
+	}
+	
+	@Override
+	public void i(String message, Throwable cause) {
+		logger.info(message, cause);
+	}
+	
+	@Override
+	public void i(String tag, String format, Object arg) {
+		logger.info("{}: " + format, tag, arg);
+	}
+	
+	@Override
+	public void i(String tag, String format, Object arg1, Object arg2) {
+		logger.info("{}: " + format, tag, arg1, arg2);
+	}
+	
+	@Override
+	public void i(String tag, String format, Object... args) {
+		logger.info(tag + ": " + format, args);
+	}
+	
+	@Override
+	public void i(String tag, String message, Throwable cause) {
+		logger.info(tag + ": " + message, cause);
 	}
 	
 	
