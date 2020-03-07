33a103a42e6549cde271dab854e37ed906813adc
Tokelon <tokelontales@gmail.com>
2020-03-07 21:39:01 +0100

    Add types for engine setup steps logic

---
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/setup/IStepsEngineSetup.java
@@ -0,0 +1,11 @@
+package com.tokelon.toktales.core.engine.setup;
+
+public interface IStepsEngineSetup extends IEngineSetup {
+
+
+	/**
+	 * @return The steps that should be used for this setup.
+	 */
+	public ISetupSteps getSteps();
+	
+}
---
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/setup/SetupSteps.java
@@ -0,0 +1,76 @@
+package com.tokelon.toktales.core.engine.setup;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SetupSteps implements ISetupSteps {
+
+
+	private double nextInsertPosition = 1d;
+	
+	private final Map<String, Double> namesToPositions;
+	private final Map<String, ISetupStep> namesToSteps;
+	
+	public SetupSteps() {
+		this(new HashMap<>(), new HashMap<>());
+	}
+	
+	public SetupSteps(Map<String, Double> namesToPositions, Map<String, ISetupStep> namesToSteps) {
+		this.namesToPositions = namesToPositions;
+		this.namesToSteps = namesToSteps;
+	}
+	
+	
+	@Override
+	public void insertStep(String name, ISetupStep step) {
+		insertStep(name, step, nextInsertPosition++);
+	}
+
+	@Override
+	public void insertStep(String name, ISetupStep step, double position) {
+		namesToPositions.put(name, position);
+		namesToSteps.put(name, step);
+	}
+
+	@Override
+	public void removeStep(String name) {
+		namesToPositions.remove(name);
+		namesToSteps.remove(name);
+	}
+
+	@Override
+	public void moveStep(String name, double newPosition) {
+		namesToPositions.put(name, newPosition);
+	}
+
+	@Override
+	public boolean hasStep(String name) {
+		return namesToSteps.containsKey(name);
+	}
+	
+	@Override
+	public boolean hasPosition(double position) {
+		return namesToPositions.containsValue(position);
+	}
+
+	@Override
+	public ISetupStep getStep(String name) {
+		return namesToSteps.get(name);
+	}
+
+	@Override
+	public double getStepPosition(String name) {
+		return namesToPositions.get(name);
+	}
+
+	@Override
+	public Map<String, Double> createStepNamesToPositions() {
+		return new HashMap<>(namesToPositions);
+	}
+
+	@Override
+	public Map<String, ISetupStep> createNamesToSteps() {
+		return new HashMap<>(namesToSteps);
+	}
+
+}
