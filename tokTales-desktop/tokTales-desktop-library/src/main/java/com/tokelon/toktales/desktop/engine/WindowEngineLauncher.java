45ed62a1e85ca6acc69b2655c9a3a5ab803a4cc6
Tokelon <tokelontales@gmail.com>
2020-03-13 19:20:50 +0100

    Extract window context management into engine setup step

--- tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/engine/WindowEngineLauncher.java
+++ tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/engine/WindowEngineLauncher.java
@@ -4,6 +4,10 @@ import com.tokelon.toktales.core.engine.EngineException;
 import com.tokelon.toktales.core.engine.IEngineContext;
 import com.tokelon.toktales.core.engine.IEngineLooper;
 import com.tokelon.toktales.core.engine.log.ILoggerFactory;
+import com.tokelon.toktales.core.engine.setup.IEngineSetup;
+import com.tokelon.toktales.core.game.IGameAdapter;
+import com.tokelon.toktales.desktop.engine.setup.DefaultDesktopEngineSetup;
+import com.tokelon.toktales.desktop.engine.setup.WindowContextManageStep;
 import com.tokelon.toktales.desktop.lwjgl.LWJGLInputProcessor;
 import com.tokelon.toktales.desktop.ui.window.IWindowContext;
 import com.tokelon.toktales.desktop.ui.window.IWindowHandler;
@@ -12,71 +16,69 @@ import com.tokelon.toktales.tools.core.sub.inject.config.IHierarchicalInjectConf
 public class WindowEngineLauncher extends DesktopEngineLauncher implements IWindowEngineLauncher {
 
 
+	public static final String STEP_WINDOW_CONTEXT_MANAGE = "STEP_WINDOW_CONTEXT_MANAGE";
+
+
 	private IEngineLooper looper;
-	
+
 	private final IWindowHandler windowHandler;
 
 	public WindowEngineLauncher(IHierarchicalInjectConfig injectConfig, IWindowHandler windowHandler) {
 		super(injectConfig, new WindowEngineLooper(windowHandler, new LWJGLInputProcessor()));
-		
+
 		this.windowHandler = windowHandler;
 	}
-	
+
 	public WindowEngineLauncher(IHierarchicalInjectConfig injectConfig, IWindowHandler windowHandler, ILoggerFactory loggerFactory) {
 		super(injectConfig, new WindowEngineLooper(windowHandler, new LWJGLInputProcessor()), loggerFactory);
-		
+
 		this.windowHandler = windowHandler;
 	}
 
 	public WindowEngineLauncher(IHierarchicalInjectConfig injectConfig, IWindowHandler windowHandler, IEngineLooper defaultLooper) {
 		super(injectConfig, defaultLooper);
-		
+
 		this.windowHandler = windowHandler;
 	}
-	
+
 	public WindowEngineLauncher(IHierarchicalInjectConfig injectConfig, IWindowHandler windowHandler, IEngineLooper defaultLooper, ILoggerFactory loggerFactory) {
 		super(injectConfig, defaultLooper, loggerFactory);
-		
+
 		this.windowHandler = windowHandler;
 	}
 
 
 	@Override
-	protected void startupEngine(IEngineContext engineContext) throws EngineException {
-		super.startupEngine(engineContext);
-		
-		windowHandler.createWindowContext(engineContext);
-	}
-	
-	@Override
-	protected void shutdownEngine(IEngineContext engineContext) throws EngineException {
-		try {
-			windowHandler.destroyWindowContext();
+	protected IEngineContext createEngine(Class<? extends IGameAdapter> adapter, IEngineSetup setup) throws EngineException {
+		getLogger().debug("Inserting setup step: {}", STEP_WINDOW_CONTEXT_MANAGE);
+		setup.getSteps().insertStep(STEP_WINDOW_CONTEXT_MANAGE, new WindowContextManageStep(windowHandler));
+
+		if(!setup.getSteps().hasStep(DefaultDesktopEngineSetup.STEP_LWJGL_PROGRAM_MANAGE)) {
+			getLogger().warn("Required setup step is not configured: {}", DefaultDesktopEngineSetup.STEP_LWJGL_PROGRAM_MANAGE);
 		}
-		finally {
-			super.shutdownEngine(engineContext);
-		}
+
+		return super.createEngine(adapter, setup);
 	}
-	
-	
+
+
 	@Override
 	protected void loop(IEngineContext engineContext, IEngineLooper defaultLooper) throws EngineException {
 		this.looper = defaultLooper;
-		
+
 		looper.loop(engineContext);
 	}
-	
+
 	@Override
 	public void terminate() {
 		if(looper != null) {
 			looper.stop();
 		}
 	}
-	
-	
+
+
 	@Override
 	public IWindowContext getWindowContext() {
 		return windowHandler.getWindowContext();
 	}
-	
+
 }
