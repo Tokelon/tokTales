4aefa6f3030d5d750cd67101216c22cdc121ed18
Tokelon <tokelontales@gmail.com>
2020-03-21 22:18:36 +0100

    Implement additional functionality for window
    - Added parameter for target monitor when changing screen modes.
    - Added getter for current monitor and whether a window is in
    fullscreen, borderless.
    - Fixed a few bugs with setting window to fullscreen or borderless.

--- tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/ui/window/IWindow.java
+++ tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/ui/window/IWindow.java
@@ -1,74 +1,77 @@
 package com.tokelon.toktales.desktop.ui.window;
 
 public interface IWindow {
-	// TODO: Add state with enum values?
+	// TODO: Add state with enum?
 
 
-	public static final int VSYNC_MODE_OFF = 0;
-	public static final int VSYNC_MODE_ON = 1;
-	public static final int VSYNC_MODE_HALF = 2;
-	
-	//public static final int INPUT_CURSOR = ;
-	//public static final int INPUT_CURSOR_DISABLED = ;
-	
-
-	
 	public void create();
 	public void show();
 	public void hide();
 	public void destroy();
 
-	
+
 	public boolean shouldClose();
-	
+
 	public void makeContextCurrent();
 	//public void detachContext(); // TODO: Add this?
-	
+
 	public void swapBuffers();
-	
-	
+
+
 	public long getId();
-	
+
 	public int getWidth();
 	public int getHeight();
-	
+
 	public int getPositionX();
 	public int getPositionY();
-	
+
 	public String getTitle();
-	
+
 	public long getMonitor();
-	
+
 	public int getSwapInterval();
-	
+
 	public int getInputMode(int mode);
-	
+
 	public int getAttribute(int attribute);
-	
-	
+
+	public boolean isFullscreen();
+
+	public boolean isBorderless();
+
+	public long getCurrentMonitor();
+
+
 	public void setSize(int width, int height);
-	
+
 	public void setPosition(int x, int y);
-	
+
 	public void setTitle(String title);
-	
+
 	public void setMonitor(long monitor);
 	public void setMonitor(long monitor, int xpos, int ypos, int width, int height, int refreshRate);
 
 	public void setSwapInterval(int interval);
-	
+
 	public void setInputMode(int mode, int value);
-	
+
 	public void setAttribute(int attribute, int value);
-	
 
+
+	public void setWindowed();
+	public void setWindowed(long monitor, int width, int height);
 	public void setWindowed(int x, int y, int width, int height);
-	
-	public void setFullscreen();
-	public void setBorderless();
 
-	
+	public void setFullscreen();
+	public void setFullscreen(long monitor);
+
+	public void setBorderless(); // Should be called setBorderlessFullscreen?
+	public void setBorderless(long monitor);
+
+
+	// Add these?
 	//public int getFrameBufferWidth();
 	//public int getFrameBufferHeight();
-	
+
 }
