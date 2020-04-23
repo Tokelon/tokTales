64371d6bf8cb5d4395385e5c2fa4dc79c95879be
Tokelon <tokelontales@gmail.com>
2020-04-23 17:14:01 +0200

    Remove modifying clear color and depth buffer in default window renderer

--- tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/lwjgl/ui/DefaultGameWindowRenderer.java
+++ tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/lwjgl/ui/DefaultGameWindowRenderer.java
@@ -14,30 +14,21 @@ public class DefaultGameWindowRenderer extends GameWindowRenderer {
 	public DefaultGameWindowRenderer(IEngineDriver engineDriver, ISurfaceManager surfaceManager) {
 		super(engineDriver, surfaceManager);
 	}
-	
-	
+
+
 	@Override
 	public void prepareFrame() {
 		super.prepareFrame();
-		
-		//GL11.glEnable(GL11.GL_DEPTH_TEST);
-		//GL11.glDepthFunc(GL11.GL_LEQUAL);
-		//GL11.glDepthMask(true);
-		
-		//GL11.glEnable(GL11.GL_ALPHA_TEST);
-		//GL11.glAlphaFunc(GL11.GL_GREATER, 0.1f); // DEPRECATED
 
-		
-		//GL11.glClearColor(0.33f, 0.59f, 0.729f, 0.0f); // Light blue
-		GL11.glClearColor(0f, 0f, 0f, 0f); // Black
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		
-		
+		// Reset the surface by clearing what's rendered
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT); // | GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_STENCIL_BUFFER_BIT
+
+		// Set the coordinates for the GL viewport
+		GL11.glViewport(0, 0, getWindow().getFrameBufferWidth(), getWindow().getFrameBufferHeight());
+
+		// Enable blending for transparency
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-
-
-		GL11.glViewport(0, 0, getWindow().getFrameBufferWidth(), getWindow().getFrameBufferHeight());
 	}
 
 }
