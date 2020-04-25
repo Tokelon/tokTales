26e2ef33909eff2b169efc8d0def6eb513a8bb96
Tokelon <tokelontales@gmail.com>
2020-04-25 23:42:43 +0200

    Implement setup step for redirecting system output to logging

--- tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/setup/DefaultEngineSetup.java
+++ tokTales-core/tokTales-core-library/src/main/java/com/tokelon/toktales/core/engine/setup/DefaultEngineSetup.java
@@ -4,10 +4,12 @@ import com.tokelon.toktales.core.engine.log.ILoggerFactory;
 import com.tokelon.toktales.core.engine.setup.steps.AddInitialGamestateSetupStep;
 import com.tokelon.toktales.core.engine.setup.steps.InitScriptingSetupStep;
 import com.tokelon.toktales.core.engine.setup.steps.LoadMainConfigSetupStep;
+import com.tokelon.toktales.core.engine.setup.steps.RedirectSystemOutputSetupStep;
 
 public class DefaultEngineSetup extends BaseEngineSetup {
 
 
+	public static final String SETUP_STEP_REDIRECT_SYSTEM_OUTPUT = "SETUP_STEP_REDIRECT_SYSTEM_OUTPUT";
 	public static final String SETUP_STEP_LOAD_MAIN_CONFIG = "SETUP_STEP_LOAD_MAIN_CONFIG";
 	public static final String SETUP_STEP_INIT_SCRIPTING = "SETUP_STEP_INIT_SCRIPTING";
 	public static final String SETUP_STEP_ADD_INITIAL_GAMESTATE = "SETUP_STEP_ADD_INITIAL_GAMESTATE";
@@ -39,6 +41,7 @@ public class DefaultEngineSetup extends BaseEngineSetup {
 	 * Called in the constructor.
 	 */
 	protected void addDefaultSteps() {
+		getSteps().insertStep(SETUP_STEP_REDIRECT_SYSTEM_OUTPUT, new RedirectSystemOutputSetupStep());
 		getSteps().insertStep(SETUP_STEP_LOAD_MAIN_CONFIG, new LoadMainConfigSetupStep());
 		getSteps().insertStep(SETUP_STEP_INIT_SCRIPTING, new InitScriptingSetupStep());
 		getSteps().insertStep(SETUP_STEP_ADD_INITIAL_GAMESTATE, new AddInitialGamestateSetupStep());
