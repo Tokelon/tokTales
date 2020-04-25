e661f97f2a26e7d9ca86492dcf6d80dddafa142b
Tokelon <tokelontales@gmail.com>
2020-04-25 23:11:42 +0200

    Implement setup step for redirecting LWJGL debug output to logging

--- tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/engine/setup/DesktopEngineSetup.java
+++ tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/engine/setup/DesktopEngineSetup.java
@@ -4,10 +4,12 @@ import com.tokelon.toktales.core.engine.setup.DefaultEngineSetup;
 import com.tokelon.toktales.desktop.lwjgl.LWJGLAddErrorCallbackSetupStep;
 import com.tokelon.toktales.desktop.lwjgl.LWJGLInitGLFWSetupStep;
 import com.tokelon.toktales.desktop.lwjgl.LWJGLRegisterCallbacksSetupStep;
+import com.tokelon.toktales.desktop.lwjgl.LWJGLSetDebugStreamSetupStep;
 
 public class DesktopEngineSetup extends DefaultEngineSetup {
 
 
+	public static final String SETUP_STEP_LWJGL_SET_DEBUG_STREAM = "SETUP_STEP_LWJGL_SET_DEBUG_STREAM";
 	public static final String SETUP_STEP_LWJGL_INIT_GLFW = "SETUP_STEP_LWJGL_INIT_GLFW";
 	public static final String SETUP_STEP_LWJGL_ADD_ERROR_CALLBACK = "SETUP_STEP_LWJGL_ADD_ERROR_CALLBACK";
 	public static final String SETUP_STEP_LWJGL_REGISTER_CALLBACKS = "SETUP_STEP_LWJGL_REGISTER_CALLBACKS";
@@ -17,6 +19,7 @@ public class DesktopEngineSetup extends DefaultEngineSetup {
 	protected void addDefaultSteps() {
 		super.addDefaultSteps();
 
+		getSteps().insertStep(SETUP_STEP_LWJGL_SET_DEBUG_STREAM, new LWJGLSetDebugStreamSetupStep());
 		getSteps().insertStep(SETUP_STEP_LWJGL_INIT_GLFW, new LWJGLInitGLFWSetupStep());
 		getSteps().insertStep(SETUP_STEP_LWJGL_ADD_ERROR_CALLBACK, new LWJGLAddErrorCallbackSetupStep());
 		getSteps().insertStep(SETUP_STEP_LWJGL_REGISTER_CALLBACKS, new LWJGLRegisterCallbacksSetupStep());
