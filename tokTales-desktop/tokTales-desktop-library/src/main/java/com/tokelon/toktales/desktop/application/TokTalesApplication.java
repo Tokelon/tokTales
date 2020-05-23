a41ad862254552bd145cc96e522234cd498e87ef
Tokelon <tokelontales@gmail.com>
2020-05-23 18:52:08 +0200

    Implement methods for accessing factories in engine application types

--- tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/application/TokTalesApplication.java
+++ tokTales-desktop/tokTales-desktop-library/src/main/java/com/tokelon/toktales/desktop/application/TokTalesApplication.java
@@ -11,12 +11,21 @@ import com.tokelon.toktales.core.game.EmptyGameAdapter;
 import com.tokelon.toktales.core.game.IGameAdapter;
 import com.tokelon.toktales.desktop.engine.DesktopLauncherFactory;
 import com.tokelon.toktales.desktop.engine.IDesktopEngineLauncher;
+import com.tokelon.toktales.desktop.engine.IDesktopLauncherFactory;
 import com.tokelon.toktales.desktop.engine.inject.MasterDesktopInjectConfig;
 import com.tokelon.toktales.desktop.engine.setup.DesktopEngineSetup;
+import com.tokelon.toktales.desktop.lwjgl.ui.IWindowContextFactory;
+import com.tokelon.toktales.desktop.ui.window.DesktopWindowContextFactory;
+import com.tokelon.toktales.desktop.ui.window.DesktopWindowFactory;
+import com.tokelon.toktales.desktop.ui.window.IWindowFactory;
 import com.tokelon.toktales.tools.core.sub.inject.config.IHierarchicalInjectConfig;
 
 public class TokTalesApplication implements IEngineApplication {
 
+
+	private final IDesktopLauncherFactory launcherFactory;
+	private final IWindowContextFactory windowContextFactory;
+	private final IWindowFactory windowFactory;
 
 	private final ILogger logger;
 
@@ -26,6 +35,10 @@ public class TokTalesApplication implements IEngineApplication {
 
 	public TokTalesApplication(ILoggerFactory loggerFactory) {
 		this.logger = loggerFactory.getLogger(getClass());
+
+		this.launcherFactory = createLauncherFactory();
+		this.windowContextFactory = createWindowContextFactory();
+		this.windowFactory = createWindowFactory();
 	}
 
 
@@ -44,7 +57,7 @@ public class TokTalesApplication implements IEngineApplication {
 			launchEngine(launcher);
 		}
 		catch(EngineException engineException) {
-			getLogger().error("Error while launching engine: ", engineException);
+			getLogger().error("Fatal error while running engine: ", engineException);
 			throw engineException;
 		}
 	}
@@ -57,7 +70,7 @@ public class TokTalesApplication implements IEngineApplication {
 
 	@Override
 	public IDesktopEngineLauncher createDefaultEngineLauncher(IHierarchicalInjectConfig defaultInjectConfig) {
-		return new DesktopLauncherFactory()
+		return getLauncherFactory()
 				.createDefaultLauncherBuilder()
 				.withInjectConfig(defaultInjectConfig)
 				.build();
@@ -76,6 +89,59 @@ public class TokTalesApplication implements IEngineApplication {
 	@Override
 	public Class<? extends IGameAdapter> getDefaultGameAdapter() {
 		return EmptyGameAdapter.class;
+	}
+
+
+	/** Convenience method for accessing a launcher factory for this application.
+	 *
+	 * @return The launcher factory for this application.
+	 */
+	public IDesktopLauncherFactory getLauncherFactory() {
+		return launcherFactory;
+	}
+
+	/** Convenience method for accessing a window context factory for this application.
+	 *
+	 * @return The window context factory for this application.
+	 */
+	public IWindowContextFactory getWindowContextFactory() {
+		return windowContextFactory;
+	}
+
+	/** Convenience method for accessing a window factory for this application.
+	 *
+	 * @return The window factory for this application.
+	 */
+	public IWindowFactory getWindowFactory() {
+		return windowFactory;
+	}
+
+
+	/** Creates a launcher factory.
+	 *
+	 * @return A new instance of the default launcher factory.
+	 * @see #getLauncherFactory()
+	 */
+	public IDesktopLauncherFactory createLauncherFactory() {
+		return new DesktopLauncherFactory();
+	}
+
+	/** Creates a window context factory.
+	 *
+	 * @return A new instance of the default window context factory.
+	 * @see #getWindowContextFactory()
+	 */
+	public IWindowContextFactory createWindowContextFactory() {
+		return new DesktopWindowContextFactory();
+	}
+
+	/** Creates a window factory.
+	 *
+	 * @return A new instance of the default window factory.
+	 * @see #getWindowFactory()
+	 */
+	public IWindowFactory createWindowFactory() {
+		return new DesktopWindowFactory();
 	}
 
 }
