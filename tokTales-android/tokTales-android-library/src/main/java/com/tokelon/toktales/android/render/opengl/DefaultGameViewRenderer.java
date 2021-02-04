fb013f4feea8534ac360c6622d1187bb03195017
Tokelon <tokelontales@gmail.com>
2021-02-04 16:53:20 +0100

    Fix Android surface not cleared by default game view renderer

--- tokTales-android/tokTales-android-library/src/main/java/com/tokelon/toktales/android/render/opengl/DefaultGameViewRenderer.java
+++ tokTales-android/tokTales-android-library/src/main/java/com/tokelon/toktales/android/render/opengl/DefaultGameViewRenderer.java
@@ -29,7 +29,7 @@ public class DefaultGameViewRenderer extends GLGameViewRenderer {
 		// Disable backface culling
 		//GLES20.glDisable(GLES20.GL_CULL_FACE);
 		
-		GLES20.glClearColor(0f, 0f, 0f, 0f); // Black
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 		
 		GLES20.glEnable(GLES20.GL_BLEND);
 		GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
